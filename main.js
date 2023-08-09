b1dc80ce0f13f796765a8bce09294b3f311dc75b
akshaysable02 <akshaysable097@gmail.com>
2023-08-09 15:30:23 +0530

    Saving the user Details on POSTMAN

--- main.js
+++ main.js
@@ -5,79 +5,115 @@ const phoneNumber = document.querySelector("#number");
 const msg = document.querySelector(".msg");
 const userList = document.querySelector("#users");
 
+
+
 myForm.addEventListener("submit", onSubmit);
-userList.addEventListener("click", removeItem);
-userList.addEventListener('click',editItem);
 
 function onSubmit(e) {
   e.preventDefault();
 
-  if (
-    nameInput.value === "" ||
-    emailInput.value === "" ||
-    phoneNumber.value === ""
-  ) {
+  if (nameInput.value === "" || emailInput.value === "" || phoneNumber.value === "") {
     msg.classList.add("error");
     msg.innerHTML = "Please enter fields";
     setTimeout(() => msg.remove(), 3000);
   } else {
-    var uniqueKey = "user" + Date.now();
-    myObj = {
+    const myObj = {
       name: nameInput.value,
       email: emailInput.value,
       number: phoneNumber.value,
     };
-    localStorage.setItem(emailInput.value, JSON.stringify(myObj));
 
-    //create li and delete button
+    axios.post('https://crudcrud.com/api/e83cd81d5a734a5d857fb20bb9872012/apponmentData',myObj)
+    .then((res)=>{
+      addUserToList(res.data)
+      console.log(res)
+    })
+    .catch((err)=>{
+      console.log(err)
+    })
 
-    const li = document.createElement("li");
-    li.appendChild(
-      document.createTextNode(
-        `Name:- ${myObj.name} ,email ID:- ${myObj.email} , Phone Number:- ${myObj.number}`
-      )
-    );
-    userList.appendChild(li);
-    var deleteItems = document.createElement("button");
-    deleteItems.className = "deleteBtn delete";
-    deleteItems.appendChild(document.createTextNode("Delete"));
-    li.appendChild(deleteItems);
 
-    var editItems = document.createElement("button");
-    editItems.className = "editBtn edit";
-    editItems.appendChild(document.createTextNode("Edit"));
-    li.appendChild(editItems);
+
+
+    // localStorage.setItem(emailInput.value, JSON.stringify(myObj));
+
+    //addUserToList(myObj);
 
     nameInput.value = "";
     emailInput.value = "";
     phoneNumber.value = "";
-
-    //console.log(localStorage)
   }
 }
-function removeItem(e) {
+
+window.addEventListener('load',loadData)
+
+
+  function loadData() {
+    for (let i = 0; i < localStorage.length; i++) {
+      const email = localStorage.key(i);
+      const storedData = JSON.parse(localStorage.getItem(email));
+      addUserToList(storedData);
+    }
+  }
+
+
+function addUserToList(userObj) {
+  const li = document.createElement("li");
+  li.setAttribute("data-id", userObj.email);
+
+  li.appendChild(
+    document.createTextNode(
+      `Name: ${userObj.name}, Email ID: ${userObj.email}, Phone Number: ${userObj.number}`
+    )
+  );
+
+  const deleteButton = createButton("Delete", "deleteBtn delete");
+  deleteButton.addEventListener("click", deleteUser);
+  li.appendChild(deleteButton);
+
+  const editButton = createButton("Edit", "editBtn edit");
+  editButton.addEventListener("click", editUser);
+  li.appendChild(editButton);
+
+  userList.appendChild(li);
+}
+
+function createButton(text, className) {
+  const button = document.createElement("button");
+  button.className = className;
+  button.appendChild(document.createTextNode(text));
+  return button;
+}
+
+function deleteUser(e) {
+  if (confirm("Are You Sure?")) {
+    const li = e.target.parentElement;
+    const email = li.getAttribute("data-id");
+    userList.removeChild(li);
+    localStorage.removeItem(email);
+  }
+}
+
+function editUser(e) {
+  if (confirm("Are You Sure?")) {
+    const li = e.target.parentElement;
+    const email = li.getAttribute("data-id");
+
+    const storedData = JSON.parse(localStorage.getItem(email));
+
+    nameInput.value = storedData.name;
+    emailInput.value = storedData.email;
+    phoneNumber.value = storedData.number;
+
+    userList.removeChild(li);
+    localStorage.removeItem(email);
+  }
+}
+
+userList.addEventListener("click", function (e) {
   if (e.target.classList.contains("delete")) {
-    if (confirm("Are You Sure?")) {
-      var li = e.target.parentElement;
-      userList.removeChild(li);
-      localStorage.removeItem(myObj.email);
-    }
-    // console.log(1)
+    deleteUser(e);
+  } else if (e.target.classList.contains("edit")) {
+    editUser(e);
   }
-}
-
-function editItem(e) {
-  if (e.target.classList.contains("edit")) {
-    if (confirm("Are You Sure?")) {
-      nameInput.value = myObj.name;
-      emailInput.value = myObj.email;
-      phoneNumber.value = myObj.number;
-
-      var li = e.target.parentElement;
-      userList.removeChild(li);
-
-      localStorage.removeItem(myObj.email);
-    }
-    // console.log(1)
-  }
-}
+});
